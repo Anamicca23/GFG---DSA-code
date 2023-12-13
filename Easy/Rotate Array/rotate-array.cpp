46f12c0f1b25a355f1e239738c28b2380e98cdcb
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-12-14 05:20:57 +0530

    Update rotate-array.cpp

--- Easy/Rotate Array/rotate-array.cpp
+++ Easy/Rotate Array/rotate-array.cpp
@@ -57,3 +57,69 @@ int main() {
 	return 0;
 }
 // } Driver Code Ends
+
+
+//----------------------------------------------------------------------------------------------------
+//2.Approach
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    //Function to reverse an array. 
+    void reverse(int arr[],int d,int n){
+        while(d<n){
+            swap(arr[d++],arr[n--]); //two pointer
+        }
+    }
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void rotateArr(int arr[], int d, int n){
+        // code here
+        if(d>n)
+        d%=n;  //to handle case when d>n.
+        reverse(arr,0,d-1); //reverse first d element
+        reverse(arr,d,n-1); //reverse remaining element
+        reverse(arr,0,n-1); //reverse entire array
+    }
+    //time complexity:O(n).
+    //space complexity:O(1).
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
