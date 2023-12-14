a73db53114cbb30d8df238e6d9d224946c824867
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-12-14 07:52:15 +0530

    Update majority-element.cpp

--- Medium/Majority Element/majority-element.cpp
+++ Medium/Majority Element/majority-element.cpp
@@ -54,3 +54,29 @@ int main(){
 }
 
 // } Driver Code Ends
+
+//------------------------------------------------------------------------------------------------------------------------
+//2.approach
+
+class Solution{
+  public:
+    int majorityElement(int a[], int size){
+        int cnt=0,elm=-1;
+         // Find a potential majority element
+        for(int i=0;i<size;i++){
+            if(cnt==0) elm=a[i];
+            if(a[i]==elm) cnt++;
+            else cnt--;
+        }
+        // now find the count of that element;
+        cnt=0;
+        for(int i=0;i<size;i++) {
+            if(a[i]==elm) cnt++;
+        }
+        // Check if the candidate is the majority element condition
+        if(cnt>floor(size/2)) return elm;
+        return -1;
+    }
+    //tc:-O(size)
+    //sc:-O(1)
+};
