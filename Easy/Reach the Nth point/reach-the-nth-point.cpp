717041ffd95b737c65b264dd2fbde8c1299b76c1
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-12-16 00:22:01 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach the Nth point/reach-the-nth-point.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int mod = 1e9 + 7;
+		int nthPoint(int n){
+		    if(n == 0 || n == 1)
+		        return 1;
+		    vector<int> dp(n+1, -1);
+		    dp[0] = 1;
+		    dp[1] = 1;
+		    for(int i=2; i<=n; i++)
+		        dp[i] = (dp[i-1] + dp[i-2]) % mod;
+		    return dp[n];
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthPoint(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
