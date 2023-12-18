1b2424ec97de8808970728f2fbb1b1acba0ac564
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2023-12-18 14:04:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        // code 
+          if(N&1){
+            int xoro=0;
+            for(int i=0;i<N;i+=2){
+                xoro^=A[i];
+            }
+            return xoro;
+        }
+        else    return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
