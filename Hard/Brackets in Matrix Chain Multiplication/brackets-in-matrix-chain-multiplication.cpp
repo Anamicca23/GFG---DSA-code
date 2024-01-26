468c7ed8ed30a5b8656553148735faf40de43972
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-01-27 00:43:17 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Brackets in Matrix Chain Multiplication/brackets-in-matrix-chain-multiplication.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+public:
+    pair<int,string> dp[27][27];
+    string matrixChainOrder(int p[], int n){
+        return f(1,n-1,p).second;
+    }
+    
+    pair<int,string> f(int i,int j,int p[]){
+        if(i==j){
+            string curr = "";
+            curr += 'A' + i-1; 
+            return {0,curr};
+        }
+        
+        if(dp[i][j].second != "") return dp[i][j];
+        
+        int val = INT_MAX;
+        string s = "";
+        
+        for(int k=i;k<j;k++){
+            pair<int,string> a =  f(i,k,p);
+            pair<int,string> b =  f(k+1,j,p);
+            int q = p[i-1]*p[j]*p[k] + a.first + b.first;
+            if(q<val){
+                val = q;
+                s = "(" + a.second + b.second + ")";
+            }
+        }
+        return dp[i][j] = {val,s};
+    }
+};
+
+//{ Driver Code Starts.
+
+int get(int p[],int n)
+{
+    int m[n][n],ans = 1e9; 
+    for (int i = 1; i < n; i++)
+        m[i][i] = 0; 
+    for (int L = 2; L < n; L++) {
+        for (int i = 1; i < n-L+1; i++){ 
+            m[i][i + L - 1] = INT_MAX; 
+            for (int k = i; k <= i + L - 2; k++){ 
+                int q = m[i][k] + m[k + 1][i + L - 1] + p[i-1] * p[k] * p[i + L - 1]; 
+                if (q < m[i][i + L - 1])
+                { 
+                    m[i][i + L - 1] = q;
+                }
+            }
+        }
+    }
+    return m[1][n-1];
+}
+
+int find(string s,int p[])
+{
+    vector<pair<int,int>> arr;
+    int ans = 0;
+    for(auto t:s)
+    {
+        if(t=='(')
+        {
+            arr.push_back({-1,-1});
+        }
+        else if(t==')')
+        {
+            pair<int,int> b=arr.back();
+            arr.pop_back();
+            pair<int,int> a=arr.back();
+            arr.pop_back();
+            arr.pop_back();
+            arr.push_back({a.first,b.second});
+            ans += a.first*a.second*b.second;
+        }
+        else 
+        {
+            arr.push_back({p[int(t-'A')],p[int(t-'A')+1]});
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int p[n];
+        for(int i = 0;i < n;i++)
+        {
+            cin>>p[i];
+        }
+        Solution ob;
+        string result = ob.matrixChainOrder(p, n);
+        if(find(result,p)==get(p,n))
+        {
+            cout<<"True"<<endl;
+        }
+        else
+        {
+            cout<<"False"<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
