a8c8bc68fe0261af79f587da7d80b95f16838725
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-01-31 22:24:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insert and Search in a Trie/insert-and-search-in-a-trie.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (26)
+
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+
+// trie node
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void) {
+    struct TrieNode *pNode = NULL;
+
+    pNode = (struct TrieNode *) malloc(sizeof(struct TrieNode));
+
+    if (pNode) {
+        int i;
+
+        pNode->isLeaf = false;
+
+        for (i = 0; i < ALPHABET_SIZE; i++) pNode->children[i] = NULL;
+    }
+
+    return pNode;
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// trie nodehttps://media.geeksforgeeks.org/img-practice/prod/courses/5/Web/Content/Clock-4_1706701529.gif
+/*
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+*/
+class Solution
+{
+    public:
+        //Function to insert string into TRIE.
+        void insert(struct TrieNode *root, string key)
+        {
+            // code here
+            for (int i=0;i<key.size();i++)
+            {
+                if (root->children[key[i]-'a']==NULL)
+                {root->children[key[i]-'a']=new TrieNode();}
+                root=root->children[key[i]-'a'];
+            }
+            root->isLeaf=true;
+        }
+        
+        //Function to use TRIE data structure and search the given string.
+        bool search(struct TrieNode *root, string key) 
+        {
+            // code here
+            for (int i=0;i<key.size();i++)
+            {
+                if (root->children[key[i]-'a']==NULL) return false;
+                root=root->children[key[i]-'a'];
+            }
+            if (root->isLeaf==true) return true;
+            return false;
+        }
+};
+
+//{ Driver Code Starts.
+
+// Driver
+int main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--) {
+        // Input keys (use only 'a' through 'z' and lower case)
+        int n;
+        cin >> n;
+        string keys[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> keys[i];
+        }
+        
+        Solution obj;
+        struct TrieNode *root = getNode();
+        // Construct trie
+        for (int i = 0; i < n; i++) obj.insert(root, keys[i]);
+
+        string abc;
+
+        cin >> abc;
+        // Search for different keys
+
+        if (obj.search(root, abc))
+            cout << "1\n";
+        else
+            cout << "0\n";
+        // char ex;
+        // cin >> ex;
+        // cout << ex << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
