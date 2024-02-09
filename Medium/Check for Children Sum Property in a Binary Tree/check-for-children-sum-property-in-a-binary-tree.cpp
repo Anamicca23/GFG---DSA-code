4a314bda5bd43b6eaa79f9f5c202484e0dc5d660
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-02-09 20:18:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check for Children Sum Property in a Binary Tree/check-for-children-sum-property-in-a-binary-tree.cpp
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){80
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    //recursion in bst
+    int isSumProperty(Node *root)
+    {
+     // Add your code here
+     //when the current node is NULL or when it's a leaf node (i.e., it has no children). In such cases, it returns 1.
+     if(!root || (!root->left && !root->right))return 1;
+          int k=0;
+         if(root->left)k=root->left->data;
+         if(root->right)k+=root->right->data;
+         //whether k is equal to the value of the current node. If it's true (i.e., the condition holds for the current node), 
+         //it recursively checks the same condition for its left and right subtrees 
+         return (k==root->data) and isSumProperty(root->left) and isSumProperty(root->right);
+    }
+  //This recursive approach traverses each node of the binary tree exactly once, making the time complexity O(n),
+  //where n is the number of nodes in the tree.
+  //The space complexity is O(height of the tree) due to the recursive function calls on the call stack.
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.isSumProperty(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
