3204f46571152c24a1546b7786c6c1012f51a7cf
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-03-04 23:27:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap the array elements/swap-the-array-elements.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Function to swap array elements
+* arr : array input
+* n : number of elements in array
+*/
+
+class Solution{
+  public:
+    void swapElements(int arr[], int n)
+    {
+        // Your code here
+        for(int i=0;i<n;i++){
+            if(i+2<n){
+                swap(arr[i],arr[i+2]);
+            }
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code to test swapElements
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int n;
+	    cin >> n;
+	    
+	    int arr[n];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr[i];
+	    }
+        Solution obj;
+        // calling function to swap the array swap elements
+	    obj.swapElements(arr, n);
+	    
+	    // Printing the modified array
+	    for(int i = 0;i<n;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
