48997d0c480820bf003df5d0b76f5441992370e0
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-07-17 20:48:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Vertical Width of a Binary Tree/vertical-width-of-a-binary-tree.cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution {
+  public:
+    int verticalWidth(Node* root) {
+        queue<pair<Node *,int>> q;
+        if(root == NULL){
+            return 0;
+        }
+        q.push({root, 0});
+        int left = 0;
+        int right = 0;
+        while(!q.empty()){
+            auto p = q.front();
+            Node* node = p.first;
+            int level = p.second;
+            q.pop();
+            
+            if(node->left != NULL){
+                left = min(left, level-1);
+                q.push({node->left, level-1});
+            }
+             if(node->right != NULL){
+                 right = max(right, level+1);
+                q.push({node->right, level+1});
+            }
+        }
+        return abs(left)+right+1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str;
+        getline(cin, str);
+        Solution obj;
+        Node *root = buildTree(str);
+
+        cout << obj.verticalWidth(root) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
