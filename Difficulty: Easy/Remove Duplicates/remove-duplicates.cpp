b352c66e3bdb3e7e86da15f779c2c0896cfecd84
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-07-28 21:53:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        unordered_set<char>st;
+        string ans;
+        for(int i=0; i<str.length();i++){
+            if(st.find(str[i])==st.end())
+            {
+                st.insert(str[i]);
+                ans+=str[i];
+        }
+    }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
