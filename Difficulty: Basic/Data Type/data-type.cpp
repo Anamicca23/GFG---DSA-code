274fe59f948aa717e63b996ee6361d6cf3cbe87f
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-09-05 03:32:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        // your code here
+        if(str=="Character")
+        return sizeof(char);
+        
+        else if (str=="Integer")
+        return sizeof(int);
+        
+        else if(str=="Long")
+        return sizeof(long);
+        
+        else if(str=="Float")
+        return sizeof(float);
+        
+       else  if(str=="Double")
+        return sizeof(double);
+        
+        else
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.dataTypeSize(str);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
