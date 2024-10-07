2d3351efad111dc99840e0513e0544a687571979
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-07 17:05:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/XOR Linked List/xor-linked-list.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <stdint.h>
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *npx;
+
+    Node(int x) {
+        data = x;
+        npx = NULL;
+    }
+};
+
+struct Node *XOR(struct Node *a, struct Node *b) {
+    return (struct Node *)((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+
+struct Node *insert(struct Node *head, int data);
+
+vector<int> getList(struct Node *head);
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        struct Node *head = NULL;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            head = insert(head, number);
+        }
+
+        vector<int> vec = getList(head);
+        for (int x : vec)
+            cout << x << " ";
+        cout << endl;
+        for (int i = vec.size() - 1; i >= 0; i--) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+    }
+    return (0);
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of linked list is as
+struct Node
+{
+    int data;
+    struct Node* npx;
+
+    Node(int x){
+        data = x;
+        npx = NULL;
+    }
+};
+
+Utility function to get XOR of two Struct Node pointer
+use this function to get XOR of two pointers
+struct Node* XOR (struct Node *a, struct Node *b)
+{
+    return (struct Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+*/
+
+// function should insert the data to the front of the list
+struct Node *insert(struct Node *head, int data)
+{
+    Node *newHead = new Node(data);
+    newHead->npx = head;
+    return newHead;
+}
+
+vector<int> getList(struct Node *head)
+{
+    vector<int> ans;
+    Node *curr = head;
+    while (curr)
+    {
+        ans.push_back(curr->data);
+        curr = curr->npx;
+    }
+    return ans;
+}
