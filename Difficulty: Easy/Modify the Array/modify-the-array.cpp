cd4daf82de44a96177c1561eaaefb79d3d4526ea
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-24 23:28:34 +0530

    Update modify-the-array.cpp

--- Difficulty: Easy/Modify the Array/modify-the-array.cpp
+++ Difficulty: Easy/Modify the Array/modify-the-array.cpp
@@ -10,14 +10,12 @@ class Solution {
   public:
     vector<int> modifyAndRearrangeArray(vector<int> &v) {
         int n = v.size();
-        for (int i = 0; i < n - 1; i++) {
+        int j = 0;
+        for (int i = 0; i < n ; i++) {
             if (v[i] == v[i + 1]) {
                 v[i] *= 2;
                 v[i + 1] = 0;
             }
-        }
-        int j = 0;
-        for (int i = 0; i < n; i++) {
             if (v[i] != 0) {
                 v[j++] = v[i];
             }
@@ -26,7 +24,7 @@ class Solution {
             v[j++] = 0;
         }
         return v;
-    }  
+    }   
 
 };
 
