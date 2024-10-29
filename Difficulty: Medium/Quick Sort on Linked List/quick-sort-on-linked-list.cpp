643af3854b15a2dacb96ecc1625dd16a83f3a333
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-10-29 21:10:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort on Linked List/quick-sort-on-linked-list.cpp
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/* a node of the singly linked list */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* A utility function to insert a node at the beginning of linked list */
+void push(struct Node*& head_ref, int new_data) {
+    /* allocate node */
+    struct Node* new_node = new Node(new_data);
+
+    /* link the old list off the new node */
+    new_node->next = head_ref;
+
+    /* move the head to point to the new node */
+    head_ref = new_node;
+}
+
+/* A utility function to print linked list */
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+/* a node of the singly linked list
+struct node
+{
+    int data;
+    struct node *next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; */
+
+// Solution class with quickSort function
+class Solution {
+  public:
+  struct Node* partition(struct Node* head, struct Node* last){
+      struct Node* i = head;
+      struct Node* j = head->next;
+      while(j!=last){
+          if(j->data<head->data){
+              i=i->next;
+              swap(i->data,j->data);
+          }
+          j=j->next;
+      }
+      swap(i->data,head->data);
+      return i;
+  }
+  void Quick_Sort(struct Node* head, struct Node* last){
+      if(head == last || head->next == last){
+          return;
+      }
+      struct Node* p = partition(head,last);
+      Quick_Sort(head,p);
+      Quick_Sort(p->next,last);
+  }
+    struct Node* quickSort(struct Node* head) {
+        Quick_Sort(head,NULL);
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        head = ob.quickSort(head);
+
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
