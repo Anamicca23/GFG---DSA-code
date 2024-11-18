b89b02f09c47c43fe98e6cc4dc412dfa1fe3b15a
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-11-18 20:22:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GFG n times/print-gfg-n-times.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printGfg(int N) {
+        // Code here
+        if(N<=0)return;
+        printGfg(N-1);
+        cout<<"GFG"<<" ";
+    }
+};
+
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printGfg(N);
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
