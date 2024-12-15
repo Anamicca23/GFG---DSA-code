41322182594219c8a17ecf91b999c78d41902f03
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2024-12-15 23:14:57 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        if(n==1) return 0;
+        for(int i=0; i<n; i++){
+            if(i==0){
+                if(arr[0]>arr[1]) return 0;
+            }else if(i==n-1){
+                if(arr[n-1]>arr[n-2]) return n-1;
+            }else{
+                if(arr[i]>arr[i-1] && arr[i]>arr[i+1]) return i;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        int idx = ob.peakElement(a);
+        int n = a.size();
+        bool f = 0;
+        if (idx < 0 and idx >= n)
+            cout << "false" << endl;
+        else {
+            if (n == 1 and idx == 0)
+                f = 1;
+            else if (idx == 0 and a[0] > a[1])
+                f = 1;
+            else if (idx == n - 1 and a[n - 1] > a[n - 2])
+                f = 1;
+            else if (a[idx] > a[idx + 1] and a[idx] > a[idx - 1])
+                f = 1;
+            else
+                f = 0;
+            if (f)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
