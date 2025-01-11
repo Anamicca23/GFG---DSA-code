d6734e9ca1c895250cd6a86bd25558c7a6f5fb71
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-01-11 23:04:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest substring with distinct characters/longest-substring-with-distinct-characters.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s){
+        vector<bool>vis(26,false);
+        int maxi=INT_MIN;
+        int l=0,r=0;
+        while(r<s.size()){
+            while(vis[s[r]-'a']==true){
+                vis[s[l]-'a']=false;
+                l++;
+            }
+            vis[s[r]-'a']=true;
+            maxi=max(maxi,r-l+1);
+            r++;
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+
+        Solution obj;
+        string s;
+        cin >> s;
+        cout << obj.longestUniqueSubstr(s) << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
