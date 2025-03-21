cb5aaf21a86567dc07f0b7f8ea92ef7c3f036322
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-03-21 06:20:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Exceptionally odd/exceptionally-odd.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+//User function template for C++
+
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        int xr=0;
+        for(int i=0; i<n; i++){
+            xr ^=arr[i];
+        }
+        return xr;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getOddOccurrence(arr, n);
+        cout << ans << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+
+// } Driver Code Ends
