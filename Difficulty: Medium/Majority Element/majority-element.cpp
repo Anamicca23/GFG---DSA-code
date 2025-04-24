15a0c7e583d1a5f3c2298630e8d385aeb3aeae08
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-04-25 04:10:28 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    int majorityElement(vector<int>& arr) {
+        // code here
+        int cnt=0,el=0;
+        for(int i:arr){
+            if(cnt==0)el=i;
+            cnt+=(i==el)?-1:1;
+        }
+        cnt = count(arr.begin(),arr.end(),el);
+        return (cnt>arr.size()/2)?el:-1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        vector<int> a, b;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.majorityElement(a) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
