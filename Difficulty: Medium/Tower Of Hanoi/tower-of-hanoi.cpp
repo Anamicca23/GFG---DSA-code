04cfcd01c560f0a0a808e392044909029b4d6799
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-06-16 17:51:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    // You need to complete this function
+
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // Your code here
+            return pow(2,n)-1;
+    }
+};
