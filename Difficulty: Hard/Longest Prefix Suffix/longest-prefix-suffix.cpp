17decfa7f9b561c4a4476703701283ea0cf59070
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2025-08-08 00:15:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -1,61 +1,28 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    int lps(string str) {
-        int n=str.length();
-        vector<int>ans(n,0);
-        int idx=0;
-        
-        for(int i=1;i<n;i++){
-            
-            if(str[i]==str[idx]){
-                ans[i]=idx+1;
-                idx++;
+    int getLPSLength(string &s) {
+        int n=s.size();
+        vector<int> ans(n,0);
+        int prefix=0,suffix=1;
+        while(suffix<n){
+            if(s[prefix]==s[suffix]){
+                ans[suffix]=prefix+1;
+                prefix++;
+                suffix++;
+                
             }
             else{
-                if(idx==0)
-                    ans[i]=0;
+                if(prefix==0){
+                    ans[suffix]=0;
+                    suffix++;
+                }
                 else{
-                    idx=ans[idx-1];
-                    i--;
-                }    
+                    prefix=ans[prefix-1];
+                }
             }
-            
         }
         return ans[n-1];
+        
     }
 };
 
-//{ Driver Code Starts.
-
-int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-
-        Solution ob;
-
-        cout << ob.lps(str) << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
