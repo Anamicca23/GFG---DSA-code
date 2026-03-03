68d5755d228ff2068563e59e7fcef52971786f4b
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-03-03 16:04:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.cpp
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.cpp
@@ -1,24 +1,39 @@
 class Solution {
   public:
-     int totalElements(vector<int> &arr) {
-        // code here
-        int n = arr.size();
+    int totalElements(vector<int> &arr) {
+        int ans = 0;
+        int f = 0,ff = 0, s = 0,sf = 0;
         int i = 0;
-        unordered_map<int, int> mp;
-        int res = 0;
-        int start = 0;
-        while(i < n){
-            mp[arr[i]]++;
-            while(mp.size() > 2){
-                mp[arr[start]]--;
-                if(mp[arr[start]] == 0){
-                    mp.erase(arr[start]);
-                }
-                start++;
+        for(int j = 0;j<arr.size();j++){
+            if(arr[j] == f) ff++;
+            else if(arr[j] == s) sf++;
+            else if(f == 0){
+                f = arr[j];
+                ff++;
             }
-            res = max(res, i - start + 1);
-            i++;
+            else if(s == 0) {
+                s = arr[j];
+                sf++;
+            }
+            else{
+                while(i < j && (ff >= 1 && sf >= 1)){
+                    if(arr[i] == f) ff--;
+                    else if(arr[i] == s) sf--;
+                    i++;
+                    if(ff == 0 || sf == 0) ans = max(ans,j-i+1);
+                }
+                if(ff == 0){
+                    ff++;
+                    f = arr[j];
+                }
+                else if(sf == 0){
+                    sf++;
+                    s = arr[j];
+                }
+            }
+            
+            ans = max(ans,j-i+1);
         }
-        return res;
+        return ans;
     }
 };
