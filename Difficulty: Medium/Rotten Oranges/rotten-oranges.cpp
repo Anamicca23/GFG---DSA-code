3d9aafde3b62d4840b145a693a03e8c8028d0583
Anamika Kumari <99593529+Anamicca23@users.noreply.github.com>
2026-03-22 19:57:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.cpp
@@ -1,72 +1,58 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-    bool isvalid(int m, int n, int i, int j){
-        if(i>=0 && i<m && j>=0 && j<n) return true;
-        return false;
-    }
   public:
-    // Function to find minimum time required to rot all oranges.
-
-int orangesRotting(vector<vector<int>>& mat) {        
-        queue<pair<pair<int,int>,int>> q;
-        int m=mat.size();
-        int n=mat[0].size();
-        int fresh=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==2) q.push({{i,j},0});
-                else if(mat[i][j]==1) fresh++;
+    int orangesRot(vector<vector<int>>& mat) {
+        int time=0,n=mat.size(),m=mat[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==2)
+                    q.push({{i,j},0});
             }
         }
-        vector<int> dim={-1,0,1,0,-1};
-        int ans=0;
+        
         while(!q.empty()){
-            auto rotten=q.front();
+            auto it = q.front();
             q.pop();
-            int x=rotten.first.first;
-            int y=rotten.first.second;
-            int t=rotten.second;
-            for(int i=0;i<dim.size()-1;i++){
-                int newx=x+dim[i];
-                int newy=y+dim[i+1];
-                if(isvalid(m,n,newx,newy) && mat[newx][newy]==1){
-                    mat[newx][newy]=2;
-                    q.push({{newx,newy},t+1});
-                    fresh--;
+            time=it.second;
+            // cout<<it.first.first<<" "<<it.first.second<<endl;
+            if(it.first.first>0){ //(i-1,j)
+                if(mat[it.first.first-1][it.first.second]==1)
+                {
+                    q.push({{it.first.first-1,it.first.second},it.second+1});
+                    mat[it.first.first-1][it.first.second]=2;
                 }
             }
-            ans=max(ans,t);
+            if(it.first.first<n-1){ //(i+1,j)
+                if(mat[it.first.first+1][it.first.second]==1)
+                {
+                    q.push({{it.first.first+1,it.first.second},it.second+1});
+                    mat[it.first.first+1][it.first.second]=2;
+                }
+            }
+            if(it.first.second>0){ //(i,j-1)
+                if(mat[it.first.first][it.first.second-1]==1)
+                {
+                    q.push({{it.first.first,it.first.second-1},it.second+1});
+                    mat[it.first.first][it.first.second-1]=2;
+                }
+            }
+            if(it.first.second<m-1){ //(i,j+1)
+                if(mat[it.first.first][it.first.second+1]==1)
+                {
+                    q.push({{it.first.first,it.first.second+1},it.second+1});
+                    mat[it.first.first][it.first.second+1]=2;
+                }
+            }
+            
         }
-        return (fresh==0)?ans:-1;
-    }
-};
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> mat(n, vector<int>(m, -1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> mat[i][j];
+        
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1) flag=false;
             }
         }
-        Solution obj;
-        int ans = obj.orangesRotting(mat);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
+        
+        return flag?time:-1;
     }
-    return 0;
-}
-// } Driver Code Ends
+};
